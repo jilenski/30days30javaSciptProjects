2170658afa1376ad8e2b081328139ccd88799845
jilenski <jtayhopon@gmail.com>
2023-10-02 20:47:37 +0800

    update depracated copy to clipboard function

--- PasswordGenerator/script.js
+++ PasswordGenerator/script.js
@@ -22,6 +22,6 @@ function createPassword() {
 }
 
 function copyPassword() {
-  passwordBox.select();
-  document.execCommand('copy');
+  navigator.clipboard.writeText(passwordBox.value);
+  alert('Password copied!');
 }
